1ebfaf198763eb3190cfc9ea24347606365097d7
Raja Hasnain Anwar <rajahasnain_570@yahoo.com>
2017-05-17 00:50:33 +0500

    Create estimateGaussian.m

---
+++ ex8/estimateGaussian.m
@@ -0,0 +1,32 @@
+function [mu sigma2] = estimateGaussian(X)
+%ESTIMATEGAUSSIAN This function estimates the parameters of a 
+%Gaussian distribution using the data in X
+%   [mu sigma2] = estimateGaussian(X), 
+%   The input X is the dataset with each n-dimensional data point in one row
+%   The output is an n-dimensional vector mu, the mean of the data set
+%   and the variances sigma^2, an n x 1 vector
+% 
+
+% Useful variables
+[m, n] = size(X);
+
+% You should return these values correctly
+mu = zeros(n, 1);
+sigma2 = zeros(n, 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the mean of the data and the variances
+%               In particular, mu(i) should contain the mean of
+%               the data for the i-th feature and sigma2(i)
+%               should contain variance of the i-th feature.
+%
+
+mu = mean(X); % a row vector with means of each column
+mu(:); % column vector
+
+% the multiplication and division with m is to make sure it divides by m instead of m-1 to calculate variance
+sigma2 = (m - 1)*var(X)/m; % a row vector with variance of each column
+sigma2(:); % column vector
+
+% =============================================================
+end
